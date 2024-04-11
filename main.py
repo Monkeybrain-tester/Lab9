8da475edd78c8934e252b92cb115328e5912e2b6
Monkeybrain-tester <93153020+Monkeybrain-tester@users.noreply.github.com>
2024-04-11 12:24:12 -0400

    I dunno man

---
+++ main.py
@@ -0,0 +1,9 @@
+def encoder(password):
+    result = ""
+    for char in password:
+        char = int(char)
+        char += 3
+        char = str(char)
+        result += char
+    return result
+
