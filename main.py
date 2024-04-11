15af39435bb0007fc1e05f0669699cb1c4096bb8
Monkeybrain-tester <93153020+Monkeybrain-tester@users.noreply.github.com>
2024-04-11 12:53:37 -0400

    Final code, all fixed up :)

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ def decoder(password):
         char = str(char)
         result += char
     return result
+
+while True:
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+    choice = input("Please enter an option:")
+
+    try:
+        choice = int(choice)
+
+    except:
+        print("thats not an int silly ;]")
+    if choice == 1:
+        passvvrd = input("Please enter your password to encode:")
+        passvvrd = encoder(passvvrd)
+        print("Your password has been encoded and stored!")
+
+    elif choice == 2:
+        print("The encoded password is ",passvvrd,", and the original password is ",decoder(passvvrd),".")
+
+    elif choice == 3:
+        exit()
+    else:
+        print("")
