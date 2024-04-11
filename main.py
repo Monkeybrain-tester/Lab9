700537c5cbfd67b4f57dd0ca26585a148e42eeb7
TheHongo <thehongouyang@gmail.com>
2024-04-11 12:43:27 -0400

    Silly Tyler I cloned your repository

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def encoder(password):
         result += char
     return result
 
+def dwcwdwr(password):
+    result = ''
+    for char in password:
+        char = int(char)
+        char -= 3
+        char = str(char)
+        result += char
+    return result
